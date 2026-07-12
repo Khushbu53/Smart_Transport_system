9b2b0c3368068c9fec9d96d977926ec0675d128d
Khushbu Prajapati <prajapatikhushbu912@gmail.com>
2026-07-12 10:43:32 +0530

    Delete backend directory

--- backend/│   ├── database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "mysql+pymysql://root:password@localhost/transitops"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
-Base = declarative_base()
